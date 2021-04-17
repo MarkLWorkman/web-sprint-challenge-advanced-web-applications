c42613198013f3bc7adcc150c4a85b64d28e3209
unknown <calanhaga@aim.com>
2021-04-16 22:35:56 -0400

    BubblePage component complete

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`api/color`)
+      .then((response) => {
+        console.log(response);
+        setColorList(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }, []);
 
   return (
     <div className="container">
@@ -15,6 +28,3 @@ const BubblePage = () => {
 };
 
 export default BubblePage;
-
-//Task List:
-//1. When the component mounts, make an axios call to retrieve all color data and push to state.
